7c9af679552069c1370eeb1f793693e9b732e5b5
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2009-05-11 00:27:14 +0000

    Import of MIYAGAWA/Module-Install-Repository-0.04 from CPAN.
    gitpan-cpan-distribution: Module-Install-Repository
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         MIYAGAWA/Module-Install-Repository-0.04.tar.gz
    gitpan-cpan-author:       MIYAGAWA
    gitpan-cpan-maturity:     released

--- inc/Module/Install/Repository.pm
+++ inc/Module/Install/Repository.pm
@@ -3,7 +3,7 @@ package Module::Install::Repository;
 
 use strict;
 use 5.008_001;
-our $VERSION = '0.01';
+our $VERSION = '0.03';
 
 use base qw(Module::Install::Base);
 
@@ -28,10 +28,25 @@ sub _find_repo {
             my $git_url = $1;
             $git_url =~ s![\w\-]+\@([^:]+):!git://$1/!;
             return $git_url;
+        } elsif (`git svn info` =~ /URL: (.*)$/m) {
+            return $1;
         }
     } elsif (-e ".svn") {
         if (`svn info` =~ /URL: (.*)$/m) {
             return $1;
+        }
+    } elsif (-e "_darcs") {
+        # defaultrepo is better, but that is more likely to be ssh, not http
+        if (my $query_repo = `darcs query repo`) {
+            if ($query_repo =~ m!Default Remote: (http://.+)!) {
+                return $1;
+            }
+        }
+
+        open my $handle, '<', '_darcs/prefs/repos' or return;
+        while (<$handle>) {
+            chomp;
+            return $_ if m!^http://!;
         }
     } elsif (-e "$ENV{HOME}/.svk") {
         # Is there an explicit way to check if it's an svk checkout?
@@ -56,4 +71,4 @@ __END__
 
 =encoding utf-8
 
-#line 95
+#line 114
